cb91dce99ed71c636c5fb98ea1711f7f5293d73b
Acid147 <norman.rauschen@web.de>
2018-10-14 14:03:37 +0200

    Documentation added

--- source/ui/CommandWindow.hx
+++ source/ui/CommandWindow.hx
@@ -7,6 +7,12 @@ class CommandWindow extends Window
 
     }
 
+    /**
+        This function adds a command to the list of commands of this window.
+
+        @param text    the shown text of the command
+        @param command the function that is executed when the command is selected
+    **/
     public function addCommand(text:String, command:Void->Void)
     {
         
