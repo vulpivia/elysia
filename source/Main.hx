fb913c49330ebb7a25ae1b4d79a78cd3a1208958
Acid147 <norman.rauschen@web.de>
2018-10-18 15:23:15 +0200

    Code style changes

--- source/Main.hx
+++ source/Main.hx
@@ -7,20 +7,26 @@ import flixel.FlxGame;
 import flixel.system.scaleModes.PixelPerfectScaleMode;
 import openfl.display.Sprite;
 
+/**
+    The game.
+**/
 class Main extends Sprite
 {
-	private static inline var SCREEN_WIDTH = 320;
-	private static inline var SCREEN_HEIGHT = 240;
-	private static inline var ZOOM = 2;
-	private static inline var UPDATE_FPS = 60;
-	private static inline var DRAW_FPS = 60;
+    static inline var SCREEN_WIDTH:Int = 320;
+    static inline var SCREEN_HEIGHT:Int = 240;
+    static inline var ZOOM:Int = 2;
+    static inline var UPDATE_FPS:Int = 60;
+    static inline var DRAW_FPS:Int = 60;
 
-	public function new()
-	{
-		super();
+    /**
+        Creates the game.
+	**/
+    public function new()
+    {
+        super();
 
-		addChild(new FlxGame(SCREEN_WIDTH, SCREEN_HEIGHT, MenuMainState, ZOOM, UPDATE_FPS, DRAW_FPS, true));
+        addChild(new FlxGame(SCREEN_WIDTH, SCREEN_HEIGHT, MenuMainState, ZOOM, UPDATE_FPS, DRAW_FPS, true));
 
-		FlxG.scaleMode = new PixelPerfectScaleMode();
-	}
+        FlxG.scaleMode = new PixelPerfectScaleMode();
+    }
 }
