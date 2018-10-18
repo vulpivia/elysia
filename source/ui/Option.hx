f8ec24a6fcd3e0d5e3ecde1cd46870afb88f221a
Acid147 <norman.rauschen@web.de>
2018-10-18 15:44:32 +0200

    Code style changes, documentation

--- source/ui/Option.hx
+++ source/ui/Option.hx
@@ -1,9 +1,16 @@
 package ui;
 
+/**
+    An option consists of a string (the text) and a function that is executed when the option is chosen.
+**/
 class Option
 {
+    /**
+        The text that is shown in the option window.
+    **/
     public var text:String;
-    private var execute:Void->Void;
+
+    var execute:Void->Void;
 
     public function new(text:String, execute:Void->Void)
     {
