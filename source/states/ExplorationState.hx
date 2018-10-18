25d507589fc99163f8bb675874ea6484d32a06b8
Acid147 <norman.rauschen@web.de>
2018-10-18 15:36:18 +0200

    Code style changes

--- source/states/ExplorationState.hx
+++ source/states/ExplorationState.hx
@@ -2,15 +2,18 @@ package states;
 
 import flixel.FlxState;
 
+/**
+    In this state, the player can walk around the map and interact with objects and NPCs.
+**/
 class ExplorationState extends FlxState
 {
-	override public function create()
-	{
-		super.create();
-	}
+    override public function create()
+    {
+        super.create();
+    }
 
-	override public function update(elapsed:Float)
-	{
-		super.update(elapsed);
-	}
+    override public function update(elapsed:Float)
+    {
+        super.update(elapsed);
+    }
 }
