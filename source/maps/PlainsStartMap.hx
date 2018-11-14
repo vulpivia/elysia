70ce9ced3cedc70a71113733ec4e801bebf33df0
Norman Rauschen <norman.rauschen@web.de>
2018-11-14 13:52:43 +0100

    Added new map

---
+++ source/maps/PlainsStartMap.hx
@@ -0,0 +1,20 @@
+package maps;
+
+import entities.events.MissionOneEvent;
+import flixel.FlxG;
+
+class PlainsStartMap extends Map
+{
+    public function new()
+    {
+        super();
+
+        layerBottom.loadMapFromCSV(AssetPaths.map_plains_start_bottom__csv, AssetPaths.tileset_map_bottom__png, Map.TILE_SIZE, Map.TILE_SIZE);
+        layerTop.loadMapFromCSV(AssetPaths.map_plains_start_top__csv, AssetPaths.tileset_map_top__png, Map.TILE_SIZE, Map.TILE_SIZE);
+        layerCollision.loadMapFromCSV(AssetPaths.map_plains_start_collision__csv, null);
+
+        FlxG.camera.setScrollBounds(0, layerBottom.width, 0, layerBottom.height);
+
+        events.push(new MissionOneEvent());
+    }
+}
