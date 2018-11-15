07c296d84402fb582899168abb1daf85447aff67
Norman Rauschen <norman.rauschen@web.de>
2018-11-15 20:16:34 +0100

    Implemented getComponent

--- source/GameObject.hx
+++ source/GameObject.hx
@@ -8,13 +8,32 @@ class GameObject
 
     var components:Array<Component>;
 
+    /**
+        Add a new component to the game object.
+
+        @param component The component that should be added.
+    **/
     public function addComponent(component:Component)
     {
         components.push(component);
     }
 
+    /**
+        Get a component from the game object.
+
+        @param t Type of the component that should be searched for.
+        @return The component that was found.
+    **/
     public function getComponent(t:Class<Component>):Component
     {
-        return null;
+        for (component in components)
+        {
+            if (Std.is(component, t))
+            {
+                return component;
+            }
+        }
+
+        throw "Required component of type " + t + " not found";
     }
 }
